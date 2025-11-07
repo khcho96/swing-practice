584d2ff6ce40a62397d914ffc98447b066e4570f
khcho96 <khcho1492@gmail.com>
2025-11-07 23:36:46 +0900

    feat: 이벤트 예제3

---
+++ src/event/EventExample3.java
@@ -0,0 +1,30 @@
+package event;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class EventExample3 extends JFrame implements ActionListener {
+
+    private JButton jButton = new JButton("버튼");
+
+    public EventExample3() {
+        setSize(500, 500);
+
+        jButton.addActionListener(new ListenerClass());
+        add(jButton);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        System.out.println("호출!");
+    }
+
+    public static void main(String[] args) {
+        new EventExample3();
+    }
+}
